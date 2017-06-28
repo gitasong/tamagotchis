34f5c288d5fb76652100dc9f0dd3602aebca5f23
Nicole Freed and Rena Rallis <nicole@>
2017-06-28 15:57:24 -0700

    modify food, love, and amtSleep properties to start at 1 (not dead); add isDead method

--- src/Tamagotchi.php
+++ src/Tamagotchi.php
@@ -5,13 +5,15 @@ class Tamagotchi
     private $food;  // number - increment
     private $love;  // love - increment
     private $sleep;  // Boolean - true or false
+    private $amt_sleep;  // amount of sleep - increment
 
-    function __construct($name, $food, $love, $sleep)
+    function __construct($name, $food, $love, $sleep, $amt_sleep)
     {
         $this->name = $name;
-        $this->food = 0;
-        $this->love = 0;
+        $this->food = 1;
+        $this->love = 1;
         $this->sleep = false;
+        $this->amt_sleep = 1;
     }
 
     function getName()
@@ -34,6 +36,11 @@ class Tamagotchi
         return $this->sleep;
     }
 
+    function getAmtSleep()
+    {
+        return $this->amt_sleep;
+    }
+
     function setName($new_name)
     {
         $this->name = (string) $new_name;
@@ -41,12 +48,12 @@ class Tamagotchi
 
     function setFood()
     {
-        $this->food = $food ++;
+        $this->food = $food++;
     }
 
     function setLove()
     {
-        $this->love = $love ++;
+        $this->love = $love++;
     }
 
     function setSleep()
@@ -54,9 +61,22 @@ class Tamagotchi
         $this->sleep = true;
     }
 
+    function setAmtSleep()
+    {
+        $this->amt_sleep = $amt_sleep++;
+    }
+
     function wake()
     {
         $this->sleep = false;
+    }
+
+    function isDead()
+    {
+        if (($this->getFood() == 0) || ($this->getLove() == 0) || ($this->getAmtSleep() == 0))
+        {
+            return "Your Tamagotchi is dead!";
+        }
     }
 
 
