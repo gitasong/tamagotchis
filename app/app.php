5333ec5e49554787c368d64e9cffb55250f7ee7c
Nicole Freed and Rena Rallis <rena@>
2017-06-28 15:23:08 -0700

    add basic project structure

---
+++ app/app.php
@@ -0,0 +1,19 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Task.php";
+
+    session_start();
+    if (empty($_SESSION['list_of_tasks'])) {
+        $_SESSION['list_of_tasks'] = array();
+    }
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+
+    return $app;
+?>
